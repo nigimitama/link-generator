bce84b23c0d21ff054c464c6d6fa98f0b42a64c1
mitama <nigimitama16@gmail.com>
2021-02-13 15:40:42 +0900

    wip: fix the structure of options.html

--- options.js
+++ options.js
@@ -1,7 +1,13 @@
+function showVersion() {
+  // TODO: manifest.jsonからversionを取得する
+  const version = 0.1;
+  let versionInfo = document.getElementById("versionInfo");
+  versionInfo.innerText = `version ${version}`
+}
 
 function main() {
   let settingsDiv = document.getElementById("settingsDiv");
-
+  showVersion();
 }
 
 main();
