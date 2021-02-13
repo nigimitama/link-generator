6aa910a85b8888267445b7c5394b439e067fc413
mitama <nigimitama16@gmail.com>
2021-02-13 17:40:12 +0900

    wip: handling multiline format

--- options.js
+++ options.js
@@ -1,3 +1,17 @@
+/*
+TODO:
+- delete button
+- handling '\n'
+
+
+*/
+function init() {
+  showVersion();
+  let updateButton = document.getElementById("updateButton");
+  updateButton.addEventListener("click", function(){ updateSettings() });
+  renderSettingsDiv()
+}
+
 function showVersion() {
   // TODO: manifest.jsonからversionを取得する
   const version = 0.1;
@@ -5,9 +19,42 @@ function showVersion() {
   versionInfo.innerText = `version ${version}`
 }
 
-function main() {
+function renderSettingsDiv() {
   let settingsDiv = document.getElementById("settingsDiv");
-  showVersion();
+  chrome.storage.sync.get(['formats'], (storage) => {
+    var formats = storage.formats;
+    for (let format of formats) {
+      let formDiv = createFormDiv();
+      let nameInput = formDiv.querySelector('.lg-name').querySelector('input');
+      nameInput.value = format['name'];
+      const isTextFormat = (format['format'] != null);
+      if (isTextFormat) {
+        let formatInput = getFormatInput(formDiv, format);
+        formatInput.value = format['format'];
+      }
+      settingsDiv.appendChild(formDiv);
+    }
+  });
 }
 
-main();
+function getFormatInput(formDiv, format) {
+  let inputs = formDiv.querySelectorAll('.lg-format');
+  console.log(inputs);
+  const isMultiLines = format['format'].includes('\n');
+  let formatInput = isMultiLines ? inputs[1] : inputs[0];
+  formatInput.hidden = false;
+  return formatInput;
+}
+
+function createFormDiv() {
+  let div = document.getElementById('formTemplate').cloneNode(true);
+  div.removeAttribute("id");
+  div.hidden = false;
+  return div
+}
+
+function updateSettings() {
+
+}
+
+init();
