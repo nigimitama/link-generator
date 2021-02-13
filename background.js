fd12386d4a3b191edd529ee4993856443552b8b9
mitama <nigimitama16@gmail.com>
2021-02-13 13:53:27 +0900

    wip: add plaintext

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 // set default setting
 var defaultFormats = [
-  { name: 'Markdown', format: '[%title%](%url%)' }
+  { name: 'Markdown', format: '[%title%](%url%)' },
+  { name: 'Plain Text', format: '%title%\n%url%' }
 ];
 
 chrome.runtime.onInstalled.addListener(() => {
